a0f8bde874cb09c33d985179aed54b58d782d1cb
RealCashProject <36957906+RealCashProject@users.noreply.github.com>
2018-03-01 13:57:20 -0300

    Initial commit

---
+++ src/Application/SignalHandler.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2015-2017, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// RealCash is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RealCash is distributed in the hope that it will be useful. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with RealCash.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <QObject>
+
+namespace WalletGui {
+
+class SignalHandler : public QObject {
+  Q_OBJECT
+  Q_DISABLE_COPY(SignalHandler)
+
+public:
+  static SignalHandler& instance();
+
+  void init();
+
+private:
+  SignalHandler();
+  ~SignalHandler();
+
+  static void sigHandler(int _signal);
+
+Q_SIGNALS:
+  void quitSignal();
+};
+
+}
---
+++ src/CryptoNoteWrapper/SemaphoreUtils.cpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2015-2017, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// RealCash is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RealCash is distributed in the hope that it will be useful. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with RealCash.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <QSemaphore>
+
+#include "SemaphoreUtils.h"
+
+namespace WalletGui {
+
+SemaphoreLocker::SemaphoreLocker(QSemaphore& _semaphore) : m_semaphore(_semaphore) {
+  m_semaphore.acquire(1);
+}
+
+SemaphoreLocker::~SemaphoreLocker() {
+  Q_ASSERT(m_semaphore.available() == 0);
+  m_semaphore.release(1);
+}
+
+void SemaphoreLocker::wait() {
+  m_semaphore.acquire(1);
+}
+
+SemaphoreUnlocker::SemaphoreUnlocker(QSemaphore& _semaphore) : m_semaphore(_semaphore) {
+  Q_ASSERT(m_semaphore.available() == 0);
+}
+
+SemaphoreUnlocker::~SemaphoreUnlocker() {
+  Q_ASSERT(m_semaphore.available() == 0);
+  m_semaphore.release(1);
+}
+
+}
