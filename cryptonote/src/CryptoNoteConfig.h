259099daecd54f992cb0fb6c2df8ba2d44eaa98f
RealCashProject <36957906+RealCashProject@users.noreply.github.com>
2018-03-02 08:34:13 -0300

    RealCash - Fix Daemon Build

--- cryptonote/src/CryptoNoteConfig.h
+++ cryptonote/src/CryptoNoteConfig.h
@@ -35,8 +35,8 @@ const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 
 // MONEY_SUPPLY - total number coins to be generated
-const uint64_t MONEY_SUPPLY				                     = UINT64_C(55000000000000000);
-const uint64_t FINAL_SUBSIDY_PER_MINUTE			             = UINT64_C(300000000);
+const uint64_t MONEY_SUPPLY				                     = UINT64_C(55000000000000000); //500M
+const uint64_t FINAL_SUBSIDY_PER_MINUTE			             = UINT64_C(30000000); //0.30 Reals
 const unsigned EMISSION_SPEED_FACTOR                         = 20;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
